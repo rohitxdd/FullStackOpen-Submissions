44a206d614df000d5f198dca37b5f54ece9a7fae
Rohit Sharma <rohitsharma998850@gmail.com>
2023-03-19 19:31:18 +0530

    completed 1.3,1.4 and 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -3,29 +3,31 @@ import Header from "./components/Header";
 import Total from "./components/Total";
 
 export default function App() {
-  const courseTitle = "Half Stack application development";
-  const courses = [
-    {
-      id: 1,
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      id: 2,
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      id: 3,
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        id: 1,
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        id: 2,
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        id: 3,
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
   return (
     <div>
-      <Header title={courseTitle} />
-      <Content contentArray={courses} />
-      <Total exercises={courses} />
+      <Header title={course.name} />
+      <Content contentArray={course.parts} />
+      <Total exercises={course.parts} />
     </div>
   );
 }
