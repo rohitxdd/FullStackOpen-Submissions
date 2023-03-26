d6329d2797f76bd337315c072b1e63f91a85c3cf
Rohit Sharma <rohitsharma998850@gmail.com>
2023-03-26 19:24:12 +0530

    initial state of the data is fetched from json server using the axios

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,17 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
-
-const initialState = [
-  { name: "Arto Hellas", number: "040-123456", id: 1 },
-  { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-  { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-  { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-];
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState(initialState);
+  const [persons, setPersons] = useState([]);
   const [newNumber, setNewNumber] = useState("");
   const [newName, setNewName] = useState("");
   const [filterText, setFilterText] = useState("");
@@ -46,6 +40,18 @@ const App = () => {
     const { value } = e.target;
     setFilterText(value);
   }
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => {
+        setPersons(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+        setPersons([]);
+      });
+  }, []);
 
   return (
     <div>
