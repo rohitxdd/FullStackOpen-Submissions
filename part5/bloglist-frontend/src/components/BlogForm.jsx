f94d9d8115562810c2d67954f39b490c8aeb7dc2
rohitxd <rohitsharma998850@gmail.com>
2024-01-09 16:05:16 +0530

    made Form component testable

---
+++ part5/bloglist-frontend/src/components/BlogForm.jsx
@@ -0,0 +1,54 @@
+import { useRef } from "react";
+import { useMessage } from "../services/MessageContext";
+
+const formStyle = {
+    margin: "5px",
+};
+export default function BlogForm({ createBlog, setFormVisibility }) {
+    const { showMessage } = useMessage();
+    const titleRef = useRef(null);
+    const authorRef = useRef(null);
+    const urlRef = useRef(null);
+
+    function handleSubmit() {
+        if (titleRef.current.value && authorRef.current.value && urlRef.current.value) {
+            const inputs = {
+                title: titleRef.current.value,
+                author: authorRef.current.value,
+                url: urlRef.current.value,
+            }
+            createBlog(inputs)
+        } else {
+            showMessage({ text: "All Fields are required", status: "error" });
+        }
+    }
+
+
+    return (
+        <div>
+            <h3>Create New Blog</h3>
+            <div style={formStyle}>
+                <span>
+                    <strong>Title: </strong>
+                </span>
+                <input type="text" ref={titleRef} name="title"></input>
+            </div>
+            <div style={formStyle}>
+                <span>
+                    <strong>Authur: </strong>
+                </span>
+                <input type="text" ref={authorRef} name="authur"></input>
+            </div>
+            <div style={formStyle}>
+                <span>
+                    <strong>Url: </strong>
+                </span>
+                <input type="text" ref={urlRef} name="url"></input>
+            </div>
+            <div style={formStyle}>
+                <button onClick={handleSubmit}>Create</button>
+                <button onClick={() => setFormVisibility(false)}>Cancel</button>
+            </div>
+        </div>
+    )
+}
