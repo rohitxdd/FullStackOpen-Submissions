30befc6efa78e12b12b1362fa5a661bcb2a442e8
rohitxd <rohitsharma998850@gmail.com>
2024-05-14 16:47:36 +0530

    exercise 8.4: Books of an author

--- part8/small-library/index.js
+++ part8/small-library/index.js
@@ -99,7 +99,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -108,7 +108,12 @@ const resolvers = {
     Query: {
         bookCount: () => books.length,
         authorCount: () => authors.length,
-        allBooks: () => books,
+        allBooks: (root, args) => {
+            if (args.author) {
+                return books.filter(book => book.author === args.author)
+            }
+            return books
+        },
         allAuthors: () => authors
     },
 
