c8b02ae811032457b61c5e9ac6199e5ed63e9a11
rohitxd <rohitsharma998850@gmail.com>
2024-05-29 15:04:28 +0530

    adding apollo/client and setting up client provider

--- part8/small-library/client/src/main.jsx
+++ part8/small-library/client/src/main.jsx
@@ -2,11 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom"
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+} from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+})
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>
 );
