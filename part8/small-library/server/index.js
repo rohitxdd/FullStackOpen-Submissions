067bd4efc7bf61733e5c2ddb405efc624bfcca79
rohitxd <rohitsharma998850@gmail.com>
2024-06-05 14:11:50 +0530

    exercise 8.14: Complete the program so that all queries and mutations work

--- part8/small-library/server/index.js
+++ part8/small-library/server/index.js
@@ -19,85 +19,6 @@ mongoose.connect(MONGODB_URI)
         process.exit(-1)
     })
 
-let authors = [
-    {
-        name: 'Robert Martin',
-        id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-        born: 1952,
-    },
-    {
-        name: 'Martin Fowler',
-        id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-        born: 1963
-    },
-    {
-        name: 'Fyodor Dostoevsky',
-        id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-        born: 1821
-    },
-    {
-        name: 'Joshua Kerievsky', // birthyear not known
-        id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-    },
-    {
-        name: 'Sandi Metz', // birthyear not known
-        id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-    },
-]
-
-let books = [
-    {
-        title: 'Clean Code',
-        published: 2008,
-        author: 'Robert Martin',
-        id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-        genres: ['refactoring']
-    },
-    {
-        title: 'Agile software development',
-        published: 2002,
-        author: 'Robert Martin',
-        id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-        genres: ['agile', 'patterns', 'design']
-    },
-    {
-        title: 'Refactoring, edition 2',
-        published: 2018,
-        author: 'Martin Fowler',
-        id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-        genres: ['refactoring']
-    },
-    {
-        title: 'Refactoring to patterns',
-        published: 2008,
-        author: 'Joshua Kerievsky',
-        id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-        genres: ['refactoring', 'patterns']
-    },
-    {
-        title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-        published: 2012,
-        author: 'Sandi Metz',
-        id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-        genres: ['refactoring', 'design']
-    },
-    {
-        title: 'Crime and punishment',
-        published: 1866,
-        author: 'Fyodor Dostoevsky',
-        id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-        genres: ['classic', 'crime']
-    },
-    {
-        title: 'Demons',
-        published: 1872,
-        author: 'Fyodor Dostoevsky',
-        id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-        genres: ['classic', 'revolution']
-    },
-]
-
-
 const typeDefs = `
   type Book {
     title: String!
@@ -134,20 +55,24 @@ const typeDefs = `
 
 const resolvers = {
     Query: {
-        bookCount: () => books.length,
-        authorCount: () => authors.length,
-        allBooks: (root, args) => {
-            return books.filter(book => {
-                const authorCondition = !args.author || book.author === args.author;
-                const genreCondition = !args.genre || book.genres.includes(args.genre);
-                return authorCondition && genreCondition;
-            });
+        bookCount: async () => Book.countDocuments({}),
+        authorCount: async () => Author.countDocuments({}),
+        allBooks: async (root, args) => {
+            let query = {};
+            if (args.author) {
+                query.author = args.author;
+            }
+            if (args.genre) {
+                query.genres = args.genre;
+            }
+            const books = await Book.find(query).populate('Author')
+            return books;
         },
-        allAuthors: () => authors
+        allAuthors: async () => Author.find({})
     },
 
     Author: {
-        bookCount: (root) => books.filter(book => book.author === root.name).length
+        bookCount: async (root) => Book.countDocuments({ author: root.id })
     },
 
     Mutation: {
@@ -170,16 +95,13 @@ const resolvers = {
                     }
                 })
             }
-
         },
-        editAuthor: (root, args) => {
-            const authorIndex = authors.findIndex(e => e.name === args.name)
-            console.log(authorIndex)
-            if (authorIndex >= 0) {
-                console.log({ ...args })
-                const newAuthorobj = { ...authors[authorIndex], born: args.setBornTo }
-                authors[authorIndex] = newAuthorobj
-                return newAuthorobj
+        editAuthor: async (root, args) => {
+            const query = { name: args.name };
+            const authorDoc = await Author.findOne(query)
+            if (authorDoc) {
+                authorDoc.born = args.setBornTo
+                return await authorDoc.save()
             }
             return null
         }
