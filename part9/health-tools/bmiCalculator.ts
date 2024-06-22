12759fc3dcb66f28865e43daa477faf50df4e72c
rohitxd <rohitsharma998850@gmail.com>
2024-06-22 23:40:50 +0530

    exercise 9.1: Body mass index

---
+++ part9/health-tools/bmiCalculator.ts
@@ -0,0 +1,20 @@
+function calculateBmi(height: number, weight: number): string {
+  if (height <= 0 || weight <= 0) {
+    return "Invalid Input";
+  }
+
+  const heightInMeters = height / 100;
+  const bmi = weight / Math.pow(heightInMeters, 2);
+
+  if (bmi <= 18.5) {
+    return "Underweight";
+  } else if (bmi > 18.5 && bmi < 25) {
+    return "Normal (healthy weight)";
+  } else if (bmi >= 25 && bmi < 30) {
+    return "Overweight";
+  } else {
+    return "Obesity";
+  }
+}
+
+console.log(calculateBmi(180, 74));
