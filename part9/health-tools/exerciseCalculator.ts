a1ea1e2cd487f3928edccbe16b92d27448d42bb6
rohitxd <rohitsharma998850@gmail.com>
2024-06-25 10:03:55 +0530

    exercise 9.7: WebExercises

--- part9/health-tools/exerciseCalculator.ts
+++ part9/health-tools/exerciseCalculator.ts
@@ -1,15 +1,3 @@
-import { argv } from "node:process";
-import { tryParseInt } from "./Utils";
-
-if (argv.length < 4) {
-  throw new Error("Invalid Arguments");
-}
-
-const hours = argv.slice(3).map((e) => tryParseInt(e));
-const target = tryParseInt(argv[2]);
-
-console.log(calculateExercises(hours, target));
-
 interface result {
   periodLength: number;
   trainingDays: number;
@@ -20,7 +8,10 @@ interface result {
   average: number;
 }
 
-function calculateExercises(hours: number[], targetAmount: number): result {
+export default function calculateExercises(
+  hours: number[],
+  targetAmount: number
+): result {
   const periodLength: number = hours.length;
   const trainingDays: number = hours.filter((n) => n > 0).length;
   const success: boolean = hours.every((n) => n >= targetAmount);
--- part9/health-tools/index.ts
+++ part9/health-tools/index.ts
@@ -1,8 +1,10 @@
 import express, { Response, Request } from "express";
 import calculateBmi from "./bmiCalculator";
 import { tryParseInt } from "./Utils";
+import calculateExercises from "./exerciseCalculator";
 
 const app = express();
+app.use(express.json());
 
 app.get("/hello", (_: Request, res: Response) => {
   return res.send("Hello Full Stack!");
@@ -20,6 +22,30 @@ app.get("/bmi", (req: Request, res: Response) => {
     return res.status(200).json({ height, weight, bmi });
   } catch (e) {
     console.log(e);
+    return res.status(400).json({ error: "malformatted parameters" });
+  }
+});
+
+app.post("/exercises", (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { daily_exercises, target } = req.body;
+
+  try {
+    if (
+      Array.isArray(daily_exercises) &&
+      !isNaN(Number(target)) &&
+      daily_exercises.every((e) => typeof e === "number")
+    ) {
+      const result = calculateExercises(
+        daily_exercises as number[],
+        target as number
+      );
+      return res.status(200).json(result);
+    } else {
+      return res.status(400).json({ error: "malformatted parameters" });
+    }
+  } catch (e) {
+    console.error(e);
     return res.status(400).json({ error: "malformatted parameters" });
   }
 });
